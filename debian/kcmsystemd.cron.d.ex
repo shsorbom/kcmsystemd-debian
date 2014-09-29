a1d8686e47ca218624d2281afc2997bc7cb78b37
Shawn Sörbom <shawn@sorbom.com>
2014-09-29 00:25:17 -0700

    /debian: * Add spelling patch * Delete cruft example files

--- debian/kcmsystemd.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the kcmsystemd package
-#
-0 4	* * *	root	[ -x /usr/bin/kcmsystemd_maintenance ] && /usr/bin/kcmsystemd_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(kcmsystemd):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="kcmsystemd" command="/usr/bin/kcmsystemd"
